a45a71367d5786227060f9a993fa3c2a447eca7f
Dennis Mensink <dennitium@gmail.com>
2018-01-22 19:20:47 +0100

    Update Hourreg.py

--- Hourreg.py
+++ Hourreg.py
@@ -12,19 +12,19 @@ while True:
     min.append(hold[1])  
               
 for i in range(len(hour)):
-	hour[i] = int(hour[i])
+    hour[i] = int(hour[i])
 sumhour = (sum(hour))
 #print(sumhour)
 
 for i in range(len(min)):
-	min[i] = int(min[i])
+    min[i] = int(min[i])
 summin= sum(min)
 #print(summin)
 while summin >= 60:
-	summin = summin - 60
-	sumhour = sumhour + 1
-	if summin < 60:
-		break
+    summin = summin - 60
+    sumhour = sumhour + 1
+    if summin < 60:
+        break
 print(sumhour,"uren",summin,"minuten")
 formin = (summin * 1.66)
 formin = int(math.ceil(formin))
